e6d2d87e53ea104816aa3c10924ab4643ed69785
Sarah Osgood <sarahjs117@gmail.com>
2020-09-19 11:58:33 -0500

    Instantiate two players and new Game to begin testing in console

---
+++ src/main.js
@@ -0,0 +1,7 @@
+
+var playerIsaac = new Player(0)
+var playerMom = new Player(0)
+var newGame = new Game(playerIsaac, playerMom)
+
+newGame.shuffleDeck(newGame.cardDeck);
+// newGame.dealHand(newGame.cardDeck);
