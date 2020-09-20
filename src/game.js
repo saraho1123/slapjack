6fd75d6c9b3b76733397a3a7bdb3166181d6c8a4
Sarah Osgood <sarahjs117@gmail.com>
2020-09-20 14:50:50 -0500

    Fix end of game with no winner to give an alert to restart game

--- src/game.js
+++ src/game.js
@@ -13,54 +13,54 @@ class Game {
       {suit: "blue-2",  value: "2", src: ".assets/blue-02.png"},
       {suit: "blue-3",  value: "3", src: ".assets/blue-03.png"},
       {suit: "blue-4",  value: "4", src: ".assets/blue-04.png"},
-      {suit: "blue-5",  value: "5", src: ".assets/blue-05.png"},
-      {suit: "blue-6",  value: "6", src: ".assets/blue-06.png"},
-      {suit: "blue-7",  value: "7", src: ".assets/blue-07.png"},
-      {suit: "blue-8",  value: "8", src: ".assets/blue-08.png"},
-      {suit: "blue-9",  value: "9", src: ".assets/blue-09.png"},
-      {suit: "blue-10",  value: "10", src: ".assets/blue-10.png"},
-      {suit: "blue-jack",  value: "jack", src: ".assets/blue-jack.png"},
-      {suit: "blue-queen",  value: "queen", src: ".assets/blue-queen.png"},
-      {suit: "blue-king",  value: "king", src: ".assets/blue-king.png"},
-      {suit: "gold-A",  value: "A", src: ".assets/gold-01.png"},
-      {suit: "gold-2",  value: "2", src: ".assets/gold-02.png"},
-      {suit: "gold-3",  value: "3", src: ".assets/gold-03.png"},
-      {suit: "gold-4",  value: "4", src: ".assets/gold-04.png"},
-      {suit: "gold-5",  value: "5", src: ".assets/gold-05.png"},
-      {suit: "gold-6",  value: "6", src: ".assets/gold-06.png"},
-      {suit: "gold-7",  value: "7", src: ".assets/gold-07.png"},
-      {suit: "gold-8",  value: "8", src: ".assets/gold-08.png"},
-      {suit: "gold-9",  value: "9", src: ".assets/gold-09.png"},
-      {suit: "gold-10",  value: "10", src: ".assets/gold-10.png"},
-      {suit: "gold-jack",  value: "jack", src: ".assets/gold-jack.png"},
-      {suit: "gold-queen",  value: "queen", src: ".assets/gold-queen.png"},
-      {suit: "gold-king",  value: "king", src: ".assets/gold-king.png"},
-      {suit: "green-A",  value: "A", src: ".assets/green-01.png"},
-      {suit: "green-2",  value: "2", src: ".assets/green-02.png"},
-      {suit: "green-3",  value: "3", src: ".assets/green-03.png"},
-      {suit: "green-4",  value: "4", src: ".assets/green-04.png"},
-      {suit: "green-5",  value: "5", src: ".assets/green-05.png"},
-      {suit: "green-6",  value: "6", src: ".assets/green-06.png"},
-      {suit: "green-7",  value: "7", src: ".assets/green-07.png"},
-      {suit: "green-8",  value: "8", src: ".assets/green-08.png"},
-      {suit: "green-9",  value: "9", src: ".assets/green-09.png"},
-      {suit: "green-10",  value: "10", src: ".assets/green-10.png"},
-      {suit: "green-jack",  value: "jack", src: ".assets/green-jack.png"},
-      {suit: "green-queen",  value: "queen", rc: ".assets/green-queen.png"},
-      {suit: "green-king",  value: "king", src: ".assets/green-king.png"},
-      {suit: "red-A",  value: "A", src: ".assets/red-01.png"},
-      {suit: "red-2",  value: "2", src: ".assets/red-02.png"},
-      {suit: "red-3",  value: "3", src: ".assets/red-03.png"},
-      {suit: "red-4",  value: "4", src: ".assets/red-04.png"},
-      {suit: "red-5",  value: "5", src: ".assets/red-05.png"},
-      {suit: "red-6",  value: "6", src: ".assets/red-06.png"},
-      {suit: "red-7",  value: "7", src: ".assets/red-07.png"},
-      {suit: "red-8",  value: "8", src: ".assets/red-08.png"},
-      {suit: "red-9",  value: "9", src: ".assets/red-09.png"},
-      {suit: "red-10",  value: "10", src: ".assets/red-10.png"},
-      {suit: "red-jack",  value: "jack", src: ".assets/red-jack.png"},
-      {suit: "red-queen",  value: "queen", src: ".assets/red-queen.png"},
-      {suit: "red-king",  value: "king", src: ".assets/red-king.png"},
+      // {suit: "blue-5",  value: "5", src: ".assets/blue-05.png"},
+      // {suit: "blue-6",  value: "6", src: ".assets/blue-06.png"},
+      // {suit: "blue-7",  value: "7", src: ".assets/blue-07.png"},
+      // {suit: "blue-8",  value: "8", src: ".assets/blue-08.png"},
+      // {suit: "blue-9",  value: "9", src: ".assets/blue-09.png"},
+      // {suit: "blue-10",  value: "10", src: ".assets/blue-10.png"},
+      // {suit: "blue-jack",  value: "jack", src: ".assets/blue-jack.png"},
+      // {suit: "blue-queen",  value: "queen", src: ".assets/blue-queen.png"},
+      // {suit: "blue-king",  value: "king", src: ".assets/blue-king.png"},
+      // {suit: "gold-A",  value: "A", src: ".assets/gold-01.png"},
+      // {suit: "gold-2",  value: "2", src: ".assets/gold-02.png"},
+      // {suit: "gold-3",  value: "3", src: ".assets/gold-03.png"},
+      // {suit: "gold-4",  value: "4", src: ".assets/gold-04.png"},
+      // {suit: "gold-5",  value: "5", src: ".assets/gold-05.png"},
+      // {suit: "gold-6",  value: "6", src: ".assets/gold-06.png"},
+      // {suit: "gold-7",  value: "7", src: ".assets/gold-07.png"},
+      // {suit: "gold-8",  value: "8", src: ".assets/gold-08.png"},
+      // {suit: "gold-9",  value: "9", src: ".assets/gold-09.png"},
+      // {suit: "gold-10",  value: "10", src: ".assets/gold-10.png"},
+      // {suit: "gold-jack",  value: "jack", src: ".assets/gold-jack.png"},
+      // {suit: "gold-queen",  value: "queen", src: ".assets/gold-queen.png"},
+      // {suit: "gold-king",  value: "king", src: ".assets/gold-king.png"},
+      // {suit: "green-A",  value: "A", src: ".assets/green-01.png"},
+      // {suit: "green-2",  value: "2", src: ".assets/green-02.png"},
+      // {suit: "green-3",  value: "3", src: ".assets/green-03.png"},
+      // {suit: "green-4",  value: "4", src: ".assets/green-04.png"},
+      // {suit: "green-5",  value: "5", src: ".assets/green-05.png"},
+      // {suit: "green-6",  value: "6", src: ".assets/green-06.png"},
+      // {suit: "green-7",  value: "7", src: ".assets/green-07.png"},
+      // {suit: "green-8",  value: "8", src: ".assets/green-08.png"},
+      // {suit: "green-9",  value: "9", src: ".assets/green-09.png"},
+      // {suit: "green-10",  value: "10", src: ".assets/green-10.png"},
+      // {suit: "green-jack",  value: "jack", src: ".assets/green-jack.png"},
+      // {suit: "green-queen",  value: "queen", rc: ".assets/green-queen.png"},
+      // {suit: "green-king",  value: "king", src: ".assets/green-king.png"},
+      // {suit: "red-A",  value: "A", src: ".assets/red-01.png"},
+      // {suit: "red-2",  value: "2", src: ".assets/red-02.png"},
+      // {suit: "red-3",  value: "3", src: ".assets/red-03.png"},
+      // {suit: "red-4",  value: "4", src: ".assets/red-04.png"},
+      // {suit: "red-5",  value: "5", src: ".assets/red-05.png"},
+      // {suit: "red-6",  value: "6", src: ".assets/red-06.png"},
+      // {suit: "red-7",  value: "7", src: ".assets/red-07.png"},
+      // {suit: "red-8",  value: "8", src: ".assets/red-08.png"},
+      // {suit: "red-9",  value: "9", src: ".assets/red-09.png"},
+      // {suit: "red-10",  value: "10", src: ".assets/red-10.png"},
+      // {suit: "red-jack",  value: "jack", src: ".assets/red-jack.png"},
+      // {suit: "red-queen",  value: "queen", src: ".assets/red-queen.png"},
+      // {suit: "red-king",  value: "king", src: ".assets/red-king.png"},
     ];
     // consider putting this in a seperate src file?
   }
@@ -83,12 +83,12 @@ class Game {
 
   updateGamePile() {
     var cardPlayed;
-    if (this.currentPlayer === undefined) { // need to change what this does!
-      this.shuffleDeck(this.gamePile);
-      this.dealHand(this.gamePile);
-      this.gamepile = [];
-      this.currentPlayer = this.playerIsaac;
-    } else if (this.currentPlayer.id === "Isaac") {
+    // if (this.currentPlayer === undefined) { // need to change what this does!
+    //   this.shuffleDeck(this.gamePile);
+    //   this.currentPlayer;
+    //   this.gamepile = [];
+    //   this.currentPlayer = this.playerIsaac;
+    if (this.currentPlayer.id === "Isaac") {
       cardPlayed = this.playerIsaac.playCard();
       this.gamePile.unshift(cardPlayed);
       this.updatePlayerTurn();
@@ -107,9 +107,13 @@ class Game {
       this.currentPlayer = this.playerMom;
     } else if (this.currentPlayer.id === "Mom" && this.playerIsaac.hand[0] !== undefined) {
       this.currentPlayer = this.playerIsaac;
-    } else {
-      debugger
-      this.currentPlayer = undefined;
+    } else if (this.playerIsaac.hand.length === 0 && this.playerMom.hand.length === 0) {
+        alert("This game is a draw. Please start again!")
+        // call a reset function here!
+    } else if (this.playerMom.hand[0] === undefined) {
+        this.currentPlayer = this.playerIsaac;
+    } else if (this.playerIsaac.hand[0] === undefined) {
+        this.currentPlayer = this.playerMom;
     }
     // This needs to be refactored using args/params (see updateGamePile())
 
@@ -132,7 +136,7 @@ class Game {
     } else if (this.gamePile[0].value === this.gamePile[2].value) {
       console.log("SANDWICH!");
       this.slapATrueCondition(player);
-      this.updateWins(playerWhoSlaps)
+      this.updateWins(player)
       this.shuffleDeck(player.hand);
     } else {
       console.log("OOPS!");
@@ -151,10 +155,12 @@ class Game {
       console.log("Mom wins!");
       this.playerMom.wins++;
       this.gamePile = [];
+      this.playerIsaac.hand = [];
     } else if (this.playerMom.hand[0] === undefined && playerWhoSlaps.id === "Isaac") {
       console.log("Isaac wins!");
       this.playerIsaac.wins++;
       this.gamePile = [];
+      this.playerMom = [];
     }
     // this.currentPlayer.wins++;
 
