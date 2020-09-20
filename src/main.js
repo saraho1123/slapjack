9f3711b2b8c98d098eb094636bc218816ec457ea
Sarah Osgood <sarahjs117@gmail.com>
2020-09-20 15:57:03 -0500

    Start set up of playing fuctions in main.js tied to DDOM and html

--- src/main.js
+++ src/main.js
@@ -10,27 +10,51 @@ var isaacWins = document.querySelector(".isaac-wins");
 var momWins = document.querySelector(".mom-wins");
 
 // event listeners:
-document.addEventListener("keydown", slap)
+document.addEventListener("keydown", slap);
+document.addEventListener("keydown", playCard);
 
 // methods from Game class that are working so far!
-currentGame.shuffleDeck(currentGame.cardDeck);
-currentGame.dealHand(currentGame.cardDeck);
-// currentGame.updateGamePile( this will need to be connected to 'p' and 'q');
-// slapATrueCondition(connect this to 'f' and 'j' keydowns);
-// updateWins( need to connect this to slapATrueCondition with correct params);
+// currentGame.shuffleDeck(currentGame.cardDeck);
+// currentGame.dealHand(currentGame.cardDeck);
+// currentGame.updateGamePile(); // connect to 'p' and 'q'
+// updateATrueConditionSlap(); // connect this to 'f' and 'j' keydowns
+// updateWins(); // connect this to slapATrueCondition with correct params
 
-// currentGame.playSlapJack(currentGame.currentPlayer);
-// will need to update this to be whether f or j is keydown
-// but for now, I don't know how to determine who slaps in the data model
-
-function slap(event) {
-  if (event.key === "f" || event.key === "F") {
-    console.log("Fslap");
-  } else if (event.key === "j" || event.key === "J") {
-    console.log("Jslap");
-  } else if (event.key === "q" || event.key === "Q") {
-    console.log("Qplaycard");
-  } else if (event.key === "p" || event.key === "P") {
-    console.log("Pplaycard");
+function playCard(event) {
+  event.preventDefault();
+  console.log('play card - Does this trigger other keys?');
+  if (event.key == 'q') {
+    console.log('Isaac card played.');
+    currentGame.currentPlayer = currentGame.playerIsaac;
+    currentGame.updateGamePile();
+  } else if (event.key == 'p') {
+    console.log('Mom card played.');
+    currentGame.currentPlayer = currentGame.playerMom;
+    currentGame.updateGamePile();
   }
 }
+
+function slap(event) {
+  event.preventDefault();
+  console.log('SLAP!');
+  if (event.key == 'f') {
+    console.log('Isaac Slap!');
+    currentGame.playSlapJack(currentGame.playerIsaac);
+  } else if (event.key == 'j') {
+    console.log('Mom Slap!');
+    currentGame.playSlapJack(currentGame.playerMom);
+  }
+}
+
+// function play() {
+//   console.log('whats up!');
+//   if (event.key === "f" || event.key === "F") {
+//     console.log("Fslap");
+//   } else if (event.key === "j" || event.key === "J") {
+//     console.log("Jslap");
+//   } else if (event.key === "q" || event.key === "Q") {
+//     console.log("Qplaycard");
+//   } else if (event.key === "p" || event.key === "P") {
+//     console.log("Pplaycard");
+//   }
+// }
