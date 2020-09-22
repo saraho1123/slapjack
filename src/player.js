7539da507bdfa609008985301b3603078fccc4d1
Sarah Osgood <sarahjs117@gmail.com>
2020-09-21 19:58:40 -0500

    Add updateWins function to player class

--- src/player.js
+++ src/player.js
@@ -4,11 +4,19 @@ class Player {
     this.wins = 0;
     this.hand = [];
     this.slapped = false;
+    this.wonThisHand = false;
   }
 
   playCard() {
     return this.hand.shift();
   }
+
+  updateWins() {
+    this.wins++;
+    this.wonThisHand = true;
+  }
+
+
 
   saveWinsToStorage() {
     // JOSN and local storage! Yay!
